e9d7fa86080be2fa5ebb28b2e20be910a1211fac
Mark Miyashita <negativetwelve@gmail.com>
2013-01-12 12:40:50 -0700

    add pagination and clean up js, also clear form on submit

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,12 +1,19 @@
-$('.comment-input').live("keydown", function(e) {
-  if (e.keyCode == 13 && !e.shiftKey) {
-    if (this.value.match("^\\s*$") == null) {
-      var parent = '#new_comment_form_' + $(this).data('id');
-      $(parent).submit();
-      e.preventDefault();
-      this.value = '';
-    } else {
-      e.preventDefault();
+$(document).ready(function() {
+  
+  $('.comment-input').live("keydown", function(e) {
+    if (e.keyCode == 13 && !e.shiftKey) {
+      if (this.value.match("^\\s*$") == null) {
+        var parent = '#new_comment_form_' + $(this).data('id');
+        $(parent).submit();
+        e.preventDefault();
+        this.value = '';
+      } else {
+        e.preventDefault();
+      }
     }
-  }
+  });
+  
+  $("#new_event").live("ajax:beforeSend", function(event, xhr, status) {
+    $('#new_status')[0].value = '';
+  });
 });
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   
   def home
     if signed_in?
-      @events = Event.where(kind: 'status').order('created_at DESC')
+      @events = Event.where(kind: 'status').paginate(page: params[:page], per_page: 30).order('created_at DESC')
     else
       @user = User.new
     end
